017f30085148c2c7d9b75f9ec4b72fd2909068d0
Ayesha Hamad <ayeshahamad2015@gmail.com>
2021-10-01 02:54:22 +0000

    updated queries

---
+++ linux_sql/sql/queries.sql
@@ -0,0 +1,44 @@
+--Query:1
+SELECT cpu_number,
+       id,
+       total_mem,
+       row_number()  OVER
+            (
+                PARTITION BY cpu_number
+                ORDER BY total_mem DESC
+            )
+FROM host_info;
+
+-- Query:2
+-- creating a function for convenience purposes, round timestamp
+CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS
+    $$
+BEGIN
+RETURN date_trunc('hour', ts) + date_part('minute', ts):: int / 5 * interval '5 min';
+END;
+$$
+LANGUAGE PLPGSQL;
+
+-- verify rount5 function
+SELECT host_id, timestamp, round5(timestamp)
+FROM host_usage;
+
+-- Answer:2
+SELECT host_id,
+       round5( host_usage.timestamp ),
+       trunc( avg( host_info.total_mem-memory_free ) / avg( host_info.total_mem ) * 100,1)
+           as avg_used_memory_percentage
+FROM host_usage
+LEFT JOIN host_info
+    ON host_usage.host_id=host_info.id
+group by round5, host_id
+order by host_id;
+
+--Query:3
+SELECT host_id,
+       round5(host_usage.timestamp),
+       COUNT(host_id) AS num_data_points
+FROM host_usage
+GROUP BY host_id, round5
+HAVING count(host_id)<=2
+ORDER BY host_id;
