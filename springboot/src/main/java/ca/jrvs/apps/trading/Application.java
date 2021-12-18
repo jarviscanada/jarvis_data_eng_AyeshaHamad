d769dd96daba0283cc0bddacd6806a325deb40dd
Ayesha Hamad <ayeshahamad2015@gmail.com>
2021-12-18 08:01:22 +0000

    dockerized app and psql

--- springboot/src/main/java/ca/jrvs/apps/trading/Application.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/Application.java
@@ -1,6 +1,6 @@
 package ca.jrvs.apps.trading;
 
-import ca.jrvs.apps.trading.controller.QuoteController;
+import ca.jrvs.apps.trading.service.QuoteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,27 +12,27 @@ import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication(
-    scanBasePackages = "ca.jrvs.apps.trading",
+    //scanBasePackages = "ca.jrvs.apps.trading",
     exclude = {JdbcTemplateAutoConfiguration.class, DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class}
 )
-class Application implements CommandLineRunner {
+public class Application implements CommandLineRunner {
 
   private Logger logger = LoggerFactory.getLogger(Application.class);
 
   //@Value("${app.init.dailyList}")
-  private String[] initDailyList;
+  //private String[] initDailyList;
 
   //why ? it should be controller
-  //@Autowired
-  private QuoteController quoteController;
-  //private QuoteService quoteService;
-
   @Autowired
-  public Application(QuoteController quoteController) {
-    this.quoteController = quoteController;
-  }
+  //private QuoteController quoteController;
+  private QuoteService quoteService;
 
-  public static void main(String[] args) throws Exception{
+  //@Autowired
+  //public Application(QuoteController quoteController) {
+  //  this.quoteController = quoteController;
+  //}
+
+  public static void main(String[] args) {
     SpringApplication springApplication = new SpringApplication(Application.class);
     springApplication.run(args);
   }
